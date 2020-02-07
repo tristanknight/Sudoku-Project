0ba300fdadc1e64ffb9eba1c93ed696b8776976a
Tristan Knight <tristanknight217@gmail.com>
2020-02-07 17:34:14 +0100

    Added game logic and UI functionality

--- sudoku.py
+++ sudoku.py
@@ -77,3 +77,109 @@ class SudokuGame(object):
             self.puzzle.append([])
             for j in range(9):
                 self.puzzle[i].append(self.start_puzzle)
+
+    def check_win(self):
+        for row in range(9):
+            if not self.check_row:
+                return False
+        for col in range(9):
+            if not self.check_col:
+                return False
+        for row in range(3):
+            for col in range(3):
+                if not self.check_square(row, col):
+                    return False
+
+        self.game_over = True  # if the above conditions are met, the game is completed
+        return True
+
+    def __check_block(self, block):
+        return set(block) == set(range(1, 10))
+
+    def __check_row(self, row):
+        return self.__check_block(self.puzzle[row])
+
+    def __check_col(self, col):
+        return self.__check_block(
+            [self.puzzle[row][col] for row in range(9)]
+        )
+
+    def __check_square(self, row, col):
+        return self.__check_block(
+            [
+                self.puzzle[r][c]
+                for r in range(row * 3, (row + 1) * 3)
+                for c in range(col * 3, (col + 1) * 3)
+            ]
+
+        )
+
+
+class SudokuUI(Frame):
+    """
+    The Tkinter UI, responsible for drawing the board and accepting user input
+    """
+    def __init__(self, parent, game):
+        self.game = game
+        self.parent = parent
+        Frame.__init__(self, parent)
+
+        self.row, self.col = 0, 0
+
+        self.__init_ui()
+
+    def __init_ui(self):
+        self.parent.title('Sudoku')
+        self.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(
+            self,
+            width=WIDTH,
+            height=HEIGHT
+        )
+
+        self.canvas.pack(fill=BOTH, side=TOP)
+        clear_button = Button(
+            self,
+            text='Clear answers',
+            command=self.__clear_answers
+        )
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        self.__draw_grid()
+        self.__draw_puzzle()
+
+        self.canvas.bind('<Button-1>', self.__cell_clicked)
+        self.canvas.bind('<Key>', self.__key_pressed)
+
+    def __draw_grid(self):
+        """
+        draws grid divided with blue lines into 3x3 squares
+        """
+        for i in range(10):
+            color = 'blue' if i % 3 == 0 else 'gray'
+
+            x0 = MARGIN + i * SIDE
+            y0 = MARGIN
+            x1 = MARGIN + i * SIDE
+            y1 = HEIGHT - MARGIN
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+            x0 = MARGIN
+            y0 = MARGIN + i * SIDE
+            x1 = WIDTH - MARGIN
+            y1 = MARGIN + i * SIDE
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+    def __draw_puzzle(self):
+        self.canvas.delete('numbers')
+        for i in range(9):
+            for j in range(9):
+                answer = self.game.puzzle[i][j]
+                if answer != 0:
+                    x = MARGIN + j * SIDE + SIDE / 2
+                    y = MARGIN + i * SIDE + SIDE / 2
+                    original = self.game.start_puzzle[i][j]
+                    color = 'black' if answer == original else 'sea green'
+                    self.canvas.create_text(
+                        x, y, text=answer, tags='numbers', fill=color
+                    )
